4c7ba53075df2b4a1775306ee3a97d50fa93f860
akrv3 <kibairl74@gmail.com>
2024-12-08 15:28:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def webhookspamm():
 app = customtkinter.CTk()
 app.geometry("500x500")
 app.title("Webhook tool - akr") 
+app.iconbitmap('logo.ico')
 
 tabview = CTkTabview(master=app)
 tabview.pack(padx=100, pady=100)
